2d81735e09f6cb8d3838fbf5550c071ee30a7fde
Mahmoud <barghoutmahmoud2@gmail.com>
2022-07-10 00:03:06 +0200

    added the basic file layout for the project added a skeleton html file, an empty style file and a js file. added 5 basic functions to the js file : add,subract,divide ,multiply and operate .

---
+++ calc.js
@@ -0,0 +1,42 @@
+function add(firstNum,secondNum)
+{
+    return firstNum+secondNum;
+}
+
+function subtract(firstNum,secondNum)
+{
+    return firstNum-secondNum;
+}
+
+function multiply(firstNum,secondNum)
+{
+    return firstNum*secondNum;
+}
+
+function divide(firstNum,secondNum)
+{
+    return firstNum/secondNum ;
+}
+
+function operate(operator,firstNum,secondNum)
+{
+switch (operator)
+{
+case'+':
+add(firstNum,secondNum);
+break;
+
+case'-':
+subtract(firstNum,secondNum);
+break;
+
+case'*':
+multiply(firstNum,secondNum);
+break;
+case'/':
+divide(firstNum,secondNum);
+break;
+}
+
+
+}
