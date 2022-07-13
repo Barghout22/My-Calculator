2dfed046ced1a57f5b757017f69ffd372f9cd140
Mahmoud <barghoutmahmoud2@gmail.com>
2022-07-13 17:11:55 +0200

    created a function to take the button pushs, store them and seperate them as numbers or operations, display them on the screen and then perform operations on two of them at a time ,display the result ,then clear the screen on the following button pushes and the start the process over.

--- calc.js
+++ calc.js
@@ -1,9 +1,9 @@
 const display=document.querySelector('.display');
 let firstLine=document.createElement('p');
 let displayValue='';
-let firstValue;
-let SecondValue;
-let operation='';
+let secondDisplayValue='';
+let operation;
+let calcResult='';
 function add(firstNum,secondNum)
 {
     return firstNum+secondNum;
@@ -48,21 +48,55 @@ function operate(operator,firstNum,secondNum)
 
 function sortEnteredButtons(button)
 {
-  if(button.classList==='numbers')
+  if (calcResult!=='')
   {
+    displayValue='';
+    secondDisplayValue='';
+    operation='';
+    firstLine.textContent='';
+    calcResult='';
+  }
+  if(button.classList.value==='numbers')
+  {
+    if(!operation)
+    {
+      displayValue+=button.getAttribute('id');
+      firstLine.textContent+=button.getAttribute('id');
+      
+    }
+    else
+    {
+      secondDisplayValue+=button.getAttribute('id');
+      firstLine.textContent+=button.getAttribute('id');
+    }
+  }
+  else if(button.classList.value==='operations')
+  {
+  if(((button.getAttribute('id'))!=='=')&&((button.getAttribute('id'))!=='clear'))
+  {
+  operation=button.getAttribute('id');
+  firstLine.textContent+=operation;
+  }
+  else if (((button.getAttribute('id'))==='=')&&((displayValue)&&(operation)&&(secondDisplayValue)))
+  {
+    calcResult=operate(operation,Number(displayValue),Number(secondDisplayValue));
+    firstLine.textContent+='='+calcResult;
 
-  }
-  else if(button.classList==='operations')
+  }  
+  else if((button.getAttribute('id'))==='clear')
   {
-    
+    displayValue='';
+    secondDisplayValue='';
+    operation='';
+    firstLine.textContent='';
   }
-  firstLine.textContent+=button.getAttribute('id');
+ }
+
+
 }
 
 
 const buttons=document.querySelectorAll('button');
 
 buttons.forEach(button=>button.addEventListener('click',()=>sortEnteredButtons(button)));
-//firstLine.textContent=displayValue;
-console.log((firstLine.textContent));
 display.appendChild(firstLine);
